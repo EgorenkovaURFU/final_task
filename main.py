7b6b18a80bc104c135a16f701bb13b22ef967d0a
Tanya <tatyana-baeva@list.ru>
2023-12-12 14:02:16 +0700

    add func for prediction

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import numpy as np
 from pydantic import BaseModel, HttpUrl
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.utils import get_file
+from tensorflow.keras.applications.efficientnet import decode_predictions
 
 
 class Item(BaseModel):
@@ -30,3 +32,23 @@ def preprocess_image(img):
     x = preprocess_input(x)
     return x
 
+@app.post("/prediction/")
+async def get_net_image_prediction(item: Item):
+    if item.url == "":
+        print(item.url)
+        return {"message": "No image link provided"}
+    
+    img = image.load_img(get_file('image', str(item.url)),target_size=(224, 224))
+  
+
+    x = preprocess_image(img)
+    
+
+    model = load_model()
+    pred = model.predict(x)
+    classes = decode_predictions(pred, top=1)[0]
+    for i in classes:
+        model_prediction = str(i[1])
+        model_prediction_confidence_score = str(i[2])
+
+        return {"model-prediction": model_prediction, "model-prediction-confidence-score": model_prediction_confidence_score}
