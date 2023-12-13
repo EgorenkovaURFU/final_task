162217c92cb62a286129f186c09c605ef7adafb1
Tanya <tatyana-baeva@list.ru>
2023-12-13 08:39:27 +0700

    change funcs

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import tensorflow as tf
 
 from fastapi import FastAPI
 
-import numpy as np
 from pydantic import BaseModel, HttpUrl
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input
-from tensorflow.keras.utils import get_file
-from tensorflow.keras.applications.efficientnet import decode_predictions
 
-from rembg import remove 
-from PIL import Image 
-
+import requests  
 import pandas as pd
+import numpy as np
+
 
 IMAGE_RES = 224 
 
@@ -22,7 +18,6 @@ labels = pd.read_csv("labels_oiseaux.csv", sep=';', header=0, index_col=0)
 
 class Item(BaseModel):
     url: HttpUrl
-    # str: str
 
 
 app = FastAPI()
@@ -30,6 +25,7 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': 'Welcome!'}
+
 
 def load_model():
     URL = 'https://www.kaggle.com/models/google/aiy/frameworks/TensorFlow1/variations/vision-classifier-birds-v1/versions/1'
@@ -39,27 +35,19 @@ def load_model():
     return model
 
 
-# def preprocess_image_to_tensor(item: Item):
-#     img = image.load_img(get_file('image', str(item.url)), target_size=(IMAGE_RES, IMAGE_RES))
-#     x = image.img_to_array(img)/255.0                                           
-#     x = np.expand_dims(x, axis=0)                                              
-#     return x
-
-# def remove_background(item: Item):
-#     #img = image.load_img(get_file('image', str(item.url)), target_size=(IMAGE_RES, IMAGE_RES))
-#     img = get_file('image', str(item.url))
-#     input = Image.open(img)
-#     output = remove(input)
-#     return output
-
-
-def preprocess_image_to_tensor(img_path):
+def preprocess_image_to_tensor(img_path='image_name.jpg'):
     img = image.load_img(img_path, target_size=(IMAGE_RES, IMAGE_RES))
     x = image.img_to_array(img)/255.0                                    
     x = np.expand_dims(x, axis=0)                                              
     return x  
-   
 
+
+def save_image(item : Item):
+    img_data = requests.get(item.url).content
+    with open('image_name.jpg', 'wb') as handler:
+        handler.write(img_data)
+
+ 
 @app.post("/prediction/")
 async def get_net_image_prediction(item: Item):
     if item.url == "":
@@ -68,12 +56,13 @@ async def get_net_image_prediction(item: Item):
     
     label='name'
 
-    print(type(output))
-    # x = preprocess_image_to_tensor(item.str)
-    # model = load_model()
-    # output = model.predict(x)
-    # prediction = np.argmax(tf.squeeze(output).numpy())
+    save_image(item)
 
-    # return {'prediction': labels[label][prediction]}
+    x = preprocess_image_to_tensor()
+    model = load_model()
+    output = model.predict(x)
+    prediction = np.argmax(tf.squeeze(output).numpy())
+
+    return {'prediction': labels[label][prediction]}
 
 
